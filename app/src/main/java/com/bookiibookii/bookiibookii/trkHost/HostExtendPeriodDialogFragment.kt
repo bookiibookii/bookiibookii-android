a6316261e03c7e3e96b3c321fe72a6184c98ab11
jung <qhrtj07@gmail.com>
2026-02-01 07:56:57 +0900

    design 통일

--- app/src/main/java/com/bookiibookii/bookiibookii/trkHost/HostExtendPeriodDialogFragment.kt
+++ app/src/main/java/com/bookiibookii/bookiibookii/trkHost/HostExtendPeriodDialogFragment.kt
@@ -1,10 +1,14 @@
 package com.bookiibookii.bookiibookii.trkHost
 
+import android.content.res.ColorStateList
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.content.ContextCompat
+import androidx.core.widget.doAfterTextChanged
 import androidx.fragment.app.DialogFragment
+import com.bookiibookii.bookiibookii.R
 import com.bookiibookii.bookiibookii.databinding.FragmentHostExtendPeriodDialogBinding
 
 
@@ -37,7 +41,26 @@ class HostExtendPeriodDialogFragment : DialogFragment() {
         binding.btnClose.setOnClickListener{dismiss()}
         binding.btnCancel.setOnClickListener{dismiss()}
         binding.btnApply.setOnClickListener{
-            // 추가
+            // 독서 기간 연장 로직 추가
+            // 버튼 비활성화도
+        }
+
+        binding.etDays.doAfterTextChanged { text ->
+            val hasInput = !text.isNullOrEmpty()
+            val context = binding.root.context
+
+            binding.btnApply.apply {
+                isEnabled = hasInput
+
+                val btBgColor = if (hasInput) R.color.grey_900 else R.color.grey_100
+                backgroundTintList = ColorStateList.valueOf(ContextCompat.getColor(context, btBgColor))
+
+                val txtColor = if (hasInput) R.color.grey_100 else R.color.grey_600
+                setTextColor(ContextCompat.getColor(context, txtColor))
+            }
+
+            val boxColor = if (hasInput) R.color.white else R.color.grey_100
+            binding.boxDays.setCardBackgroundColor(ContextCompat.getColor(context, boxColor))
         }
     }
 
