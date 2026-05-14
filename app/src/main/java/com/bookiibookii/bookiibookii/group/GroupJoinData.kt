1e92cc1d96bf46097032ba885341cebd51fafa42
jung <qhrtj07@gmail.com>
2026-05-15 00:30:05 +0900

    그룹 로직 우선 삭제

--- app/src/main/java/com/bookiibookii/bookiibookii/group/GroupJoinData.kt
+++
@@ -1,9 +0,0 @@
-package com.bookiibookii.bookiibookii.group
-data class GroupJoinData(
-    val id: Int,                 // 유저(신청) 고유 ID
-    val profileImgUrl: String?,  // [변경] 서버 이미지 URL (없으면 null)
-    val nickname: String,
-    val date: String,            // ex: "2026.01.29"
-    val intro: String,           // 신청 한마디
-    val tags: List<String>       // [#태그1, #태그2 ...] (이미 한글로 변환된 상태로 받음)
-)
