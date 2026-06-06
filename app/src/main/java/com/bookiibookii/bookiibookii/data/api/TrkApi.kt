2b2f8c8ec5997bcd4ab220d0b7a036c8215cd0ce
jungee <qhrtj07@gmail.com>
2026-06-07 01:47:08 +0900

    트래커 구버전 api 삭제

--- app/src/main/java/com/bookiibookii/bookiibookii/data/api/TrkApi.kt
+++ app/src/main/java/com/bookiibookii/bookiibookii/data/api/TrkApi.kt
@@ -1,13 +1,8 @@
 package com.bookiibookii.bookiibookii.data.api
 
 import com.bookiibookii.bookiibookii.data.model.common.ApiResponse
-import com.bookiibookii.bookiibookii.data.model.tracker.TrackerCompletionResponse
 import com.bookiibookii.bookiibookii.data.model.tracker.TrackerListResDTO
-import com.bookiibookii.bookiibookii.data.model.tracker.TrackerDeliveryRequest
-import com.bookiibookii.bookiibookii.data.model.tracker.TrackerDeliveryResponse
 import com.bookiibookii.bookiibookii.data.model.tracker.TrackerDetailResDTO
-import com.bookiibookii.bookiibookii.data.model.tracker.TrackerExtensionResponse
-import com.bookiibookii.bookiibookii.data.model.tracker.TrackerImagePresignedUrlResponse
 import com.bookiibookii.bookiibookii.data.model.tracker.BookReviewReqDTO
 import com.bookiibookii.bookiibookii.data.model.tracker.BookReviewResDTO
 import com.bookiibookii.bookiibookii.data.model.tracker.DeliveryAddressDirectUpdateReqDTO
@@ -23,13 +18,6 @@ import com.bookiibookii.bookiibookii.data.model.tracker.ReadingPeriodUpdateReqDT
 import com.bookiibookii.bookiibookii.data.model.tracker.ReadingPeriodUpdateResDTO
 import com.bookiibookii.bookiibookii.data.model.tracker.ReadingProgressReqDTO
 import com.bookiibookii.bookiibookii.data.model.tracker.ReadingProgressResDTO
-import com.bookiibookii.bookiibookii.data.model.tracker.TrackerMeetingRequest
-import com.bookiibookii.bookiibookii.data.model.tracker.TrackerMeetingResponse
-import com.bookiibookii.bookiibookii.data.model.tracker.TrackerReadingResponse
-import com.bookiibookii.bookiibookii.data.model.tracker.TrackerReceptionImageResponse
-import com.bookiibookii.bookiibookii.data.model.tracker.TrackerReceptionRequest
-import com.bookiibookii.bookiibookii.data.model.tracker.TrackerReceptionResponse
-import com.bookiibookii.bookiibookii.data.model.tracker.TrackerShippingImageResponse
 import retrofit2.Response
 import retrofit2.http.Body
 import retrofit2.http.GET
@@ -37,7 +25,6 @@ import retrofit2.http.PATCH
 import retrofit2.http.POST
 import retrofit2.http.PUT
 import retrofit2.http.Path
-import retrofit2.http.Query
 
 interface TrkApi {
     @GET("/api/me/trackers")
@@ -132,68 +119,4 @@ interface TrkApi {
     suspend fun patchPartnerReceive(
         @Path("groupId") groupId: Long,
     ): Response<ApiResponse<String>>
-
-    @POST("/api/groups/{groupId}/tracker/delivery")
-    suspend fun postTrackerShippingStart(
-        @Path("groupId") groupId: Long,
-        @Body request: TrackerDeliveryRequest
-    ): ApiResponse<TrackerDeliveryResponse>
-
-    @POST("/api/groups/{groupId}/tracker/images/presigned-url")
-    suspend fun getTrackerImagePresignedUrl(
-        @Path("groupId") groupId: Long
-    ): ApiResponse<TrackerImagePresignedUrlResponse>
-
-    @PATCH("/api/groups/{groupId}/tracker/reception")
-    suspend fun patchTrackerReceive(
-        @Path("groupId") groupId: Long,
-        @Body request: TrackerReceptionRequest
-    ): ApiResponse<TrackerReceptionResponse>
-
-    @PATCH("/api/groups/{groupId}/tracker/reading")
-    suspend fun patchTrackerReadingStart(
-        @Path("groupId") groupId: Long
-    ): ApiResponse<TrackerReadingResponse>
-
-    @PATCH("/api/groups/{groupId}/tracker/extension")
-    suspend fun patchTrackerExtension(
-        @Path("groupId") groupId: Long,
-        @Query("days") days: Int = 3
-    ): ApiResponse<TrackerExtensionResponse>
-
-    @PATCH("/api/groups/{groupId}/tracker/done")
-    suspend fun patchTrackerDone(
-        @Path("groupId") groupId: Long
-    ): ApiResponse<TrackerCompletionResponse>
-
-    @GET("/api/groups/{groupId}/tracker/images/delivery")
-    suspend fun getTrackerCheckShippingImage(
-        @Path("groupId") groupId: Long
-    ): ApiResponse<TrackerShippingImageResponse>
-
-    @GET("/api/groups/{groupId}/tracker/images/received")
-    suspend fun getTrackerCheckReceivedImage(
-        @Path("groupId") groupId: Long
-    ): ApiResponse<TrackerReceptionImageResponse>
-
-    @PATCH("api/groups/{groupId}/tracker/meetings")
-    suspend fun makeMeeting(
-        @Path("groupId") groupId: Long,
-        @Body request: TrackerMeetingRequest
-    ): Response<TrackerDetailResDTO>
-
-    @GET("/api/groups/{groupId}/tracker/meetings")
-    suspend fun getTrackerMeeting(
-        @Path("groupId") groupId: Long
-    ): ApiResponse<TrackerMeetingResponse>
-
-    @PATCH("/api/groups/{groupId}/tracker/meetings/completion")
-    suspend fun patchMeetingComplete(
-        @Path("groupId") groupId: Long
-    ): Response<TrackerDetailResDTO>
-
-    @PATCH("/api/groups/{groupId}/tracker/reception/verification")
-    suspend fun patchConfirmReception(
-        @Path("groupId") groupId: Long
-    ): ApiResponse<TrackerDetailResDTO>
 }
--- app/src/main/java/com/bookiibookii/bookiibookii/data/model/tracker/TrackerImagePresignedUrl.kt
+++
@@ -1,6 +0,0 @@
-package com.bookiibookii.bookiibookii.data.model.tracker
-
-data class TrackerImagePresignedUrlResponse(
-    val s3Key: String,
-    val presignedPutUrl: String
-)
